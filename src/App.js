fc27229bc0fd0d1ef6e390e90cec1a8a53b6af1b
thenglong <thenglongheng2k@gmail.com>
2021-06-25 00:22:43 +0700

    v1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { a } from "@long/test"
+
+import logo from "./logo.svg"
+
+import "./App.css"
 
 function App() {
   return (
     <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{a}</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>{" "}
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div>
+              <div>
+                <div>
+                  <div></div>
+                </div>
+              </div>
+            </div>
+            Learn React
+          </a>
+        </header>
+      </header>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,7 +55,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/config/aliases.js
@@ -0,0 +1,19 @@
+const aliases = (prefix = `src`) => ({
+  "@long": `${prefix}/test`,
+  "@atoms": `${prefix}/components/atoms`,
+  "@molecules": `${prefix}/components/molecules`,
+  "@organisms": `${prefix}/components/organisms`,
+  "@templates": `${prefix}/components/templates`,
+  "@components": `${prefix}/components`,
+  "@config": `${prefix}/config`,
+  "@enums": `${prefix}/enums`,
+  "@hooks": `${prefix}/hooks`,
+  "@icons": `${prefix}/components/atoms/Icons`,
+  "@styles": `${prefix}/styles`,
+  "@utils": `${prefix}/utils`,
+  "@state": `${prefix}/state`,
+  "@types": `${prefix}/types`,
+  "@storybookHelpers": `../.storybook/helpers`,
+})
+
+module.exports = aliases
