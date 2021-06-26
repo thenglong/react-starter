86d05911bbfe28d267ac4b514d73e50023abaa88
thenglong <thenglongheng2k@gmail.com>
2021-06-26 12:15:46 +0700

    modified:   .eslintrc.yml 	new file:   .idea/.gitignore 	new file:   .idea/codeStyles/Project.xml 	new file:   .idea/codeStyles/codeStyleConfig.xml 	new file:   .idea/inspectionProfiles/Project_Default.xml 	new file:   .idea/jsLinters/eslint.xml 	new file:   .idea/modules.xml 	new file:   .idea/prettier.xml 	new file:   .idea/react-starter.iml 	new file:   .idea/vcs.xml 	modified:   package.json 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import logo from "./logo.svg"
 import "./App.css"
+import { Grid, Typography } from "@material-ui/core"
+
+import logo from "./logo.svg"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Typography bgcolor="beige"></Typography>
+        <Grid xs={10}></Grid>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 2
         </a>
       </header>
     </div>
