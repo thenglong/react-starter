91b6b57af80bfb35d7982bef42a845246a052f2c
thenglong <long@long.com>
2021-08-25 23:12:37 +0700

    fix-logger

--- src/App.test.js
+++ src/App.test.js
@@ -2,9 +2,9 @@ import { render, screen } from "@testing-library/react"
 
 import App from "./App"
 
-test("renders learn react link", () => {
+test("renders with logger link", () => {
   render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
+  const linkElement = screen.getByText(/With logger/i)
   expect(linkElement).toBeInTheDocument()
 })
 
